54e46265cb61eacc1af6874d927db9383a81d74c
victoria <victori.a3@mail.ru>
2023-12-11 22:22:30 +0300

    cookbook done

---
+++ task_1-2.py
@@ -0,0 +1,36 @@
+with open('recipes.txt', encoding='utf-8') as f:
+    cook_book = {}
+    for line in f:
+        title = line.strip()
+        component = int(f.readline())
+        components_list = []
+        for item in range(component):
+            component_dict = {} 
+            ingredient_name, quantity, measure = f.readline().split('|')
+            component_dict['ingredient_name'] = ingredient_name.strip()
+            component_dict['quantity'] = int(quantity.strip())
+            component_dict['measure'] = measure.strip()
+            components_list.append(component_dict)
+            cook_book[title] = components_list
+        f.readline()
+    
+def get_shop_list_by_dishes(dishes, person_count):
+    shoping_list = {}
+    for dish in dishes:
+        if dish in cook_book:
+                component = cook_book.get(dish)
+                for ingredient in component:
+                    measure_dict = {}
+                    measure_dict['measure'] = ingredient['measure']
+                    count = ingredient['quantity'] * person_count
+                    if ingredient['ingredient_name'] not in shoping_list.keys():
+                        measure_dict['quantity'] = count
+                    else:
+                        count += count
+                        measure_dict['quantity'] = count
+                    shoping_list[ingredient['ingredient_name']] = measure_dict
+        else:
+            print(f'{dish}: рецепт не найден')
+    return shoping_list
+
+print(get_shop_list_by_dishes(['Фахитос', 'Запеченный картофель', 'Омлет'], 2))
